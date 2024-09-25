6ea5ecfffadbd98960c2aaa7bcf2f8ba228e6313
mdwiandh <2310631230039@student.unsika.ac.id>
2024-09-25 14:29:14 +0700

    Create block.py

---
+++ block.py
@@ -0,0 +1,92 @@
+import time
+import hashlib
+
+class Block:
+    def init(self, index, previous_hash, timestamp, data, nonce=0):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.timestamp = timestamp
+        self.data = data
+        self.nonce = nonce
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        block_string = f"{self.index}{self.previous_hash}{self.timestamp}{self.data}{self.nonce}"
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
+    def repr(self):
+        return f"Block({self.index}, {self.previous_hash}, {self.timestamp}, {self.data}, {self.nonce})"
+
+class Blockchain:
+    def init(self):
+        self.chain = [self.create_genesis_block()]
+        self.difficulty = 4  # Mengatur tingkat kesulitan (jumlah nol di depan hash)
+
+    def create_genesis_block(self):
+        # Blok pertama dalam blockchain, disebut sebagai blok genesis.
+        return Block(0, "0", time.time(), "Genesis Block")
+
+    def get_latest_block(self):
+        # Mengembalikan blok terakhir di dalam blockchain.
+        return self.chain[-1]
+
+    # Note: There might be additional methods like add_block, is_chain_valid, etc., 
+    # but they are not visible in the provided images.
+
+# Usage example (not shown in images, but typically included):
+# blockchain = Blockchain()
+# blockchain.add_block(Block(1, blockchain.get_latest_block().hash, time.time(), "Transaction Data"))
+def add_block(self, new_block):
+    """
+    Menambahkan blok baru ke dalam blockchain setelah memvalidasi proof of work.
+    """
+    new_block.previous_hash = self.get_last_block().hash
+    new_block.hash = self.calculate_hash(new_block)
+    self.chain.append(new_block)
+
+def proof_of_work(self, block):
+    """
+    Proses penambangan sederhana yang mencoba menemukan hash yang valid (dengan kesulitan tertentu).
+    """
+    block.nonce = 0
+    calculated_hash = block.calculate_hash()
+    while not calculated_hash.startswith('0' * self.difficulty):
+        block.nonce += 1
+        calculated_hash = block.calculate_hash()
+    return calculated_hash
+def is_chain_valid(self):
+    """
+    Memeriksa validitas blockchain dengan memverifikasi setiap blok dan hash yang saling merujuk.
+    """
+    for i in range(1, len(self.chain)):
+        current_block = self.chain[i]
+        previous_block = self.chain[i - 1]
+
+        # Periksa apakah hash blok saat ini benar
+        if current_block.hash != current_block.calculate_hash():
+            print(f"Hash of block {i} is invalid!")
+            return False
+
+        # Periksa apakah blok ini merujuk ke hash blok sebelumnya dengan benar
+        if current_block.previous_hash != previous_block.hash:
+            print(f"Previous hash of block {i} is invalid!")
+            return False
+
+    return True
+
+# Demonstrasi
+
+# Membuat blockchain baru
+my_blockchain = Blockchain()
+
+# Menambah beberapa blok baru ke dalam blockchain
+my_blockchain.add_block(Block(1, "", time.time(), "Transaksi 1"))
+my_blockchain.add_block(Block(2, "", time.time(), "Transaksi 2"))
+my_blockchain.add_block(Block(3, "", time.time(), "Transaksi 3"))
+
+# Cetak seluruh blok di dalam blockchain
+for block in my_blockchain.chain:
+    print(block)
+
+# Memeriksa apakah blockchain valid
+print(f"Apakah blockchain valid? {my_blockchain.is_chain_valid()}")
